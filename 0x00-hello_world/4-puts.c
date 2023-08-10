fee1a88d0126daadd66d0daaedba4d5a7a0bc9b5
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2023-08-10 13:51:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * pust function print text anthoer line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
