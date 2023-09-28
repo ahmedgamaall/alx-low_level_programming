0350c338284f21fcaa069f061701f9b236e8ecf7
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2023-09-28 19:11:44 +0300

    create 0-binary_to_uint.c file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,8 +5,8 @@
  * @b is pointing to a string of 0 and 1 chars
  *
  * Return: the converted number  or 0 if
- * there is one or more chars in the string b that is not 0 or 1
- * b is NULL
+ * - there is one or more chars in the string b that is not 0 or 1
+ * - b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
