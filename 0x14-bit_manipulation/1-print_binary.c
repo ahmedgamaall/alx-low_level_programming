dbc7e0f762bc7e09cb84b7e026e9857bca8b1c8d
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2023-10-26 12:18:57 -0700

    create 1-print_binary.c file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _pow - function calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+unsigned long int _power_num(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int i;
+
+	num = 1;
+	for (i = 1; i <= power; i++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, checker;
+	char flag;
+
+	flag = 0;
+	divisor = _power_num(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		checker = n & divisor;
+		if (checker == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
+}
