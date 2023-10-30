6567a1d7db4a977f3223232748177af7eabc95e5
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2023-10-30 11:33:45 -0700

    create 0-read_textfile.c file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,11 +5,8 @@
  * @filename: filename.
  * @letters: numbers of letters printed.
  *
- * returns the actual number of letters it could read and print
- * if the file can not be opened or read, return 0
- * if filename is NULL return 0
- * if write fails or does not write the expected amount of bytes, return 0
- */
+ * Return: the actual number of letters it could read and print.
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
