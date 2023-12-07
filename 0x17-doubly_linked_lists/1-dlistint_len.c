cd8d39814254aa45d1ea8911824d298969019cea
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2023-12-07 06:31:05 -0800

    create 1-dlistint_len.c file

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in
+ * a double linked list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int c;
+
+	c = 0;
+
+	if (h == NULL)
+		return (c);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		c++;
+		h = h->next;
+	}
+
+	return (c);
+}
