0ee00f53bdcbd760745fe92580a4bc8f91568e8f
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2023-12-12 08:49:29 -0800

    create 100-operations.so file

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+*add - add two int
+* @a: first int
+* @b: second int
+* 
+* Return: addition
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - sub two int
+* @a: first int
+* @b: second int
+* 
+* Return: subtraction
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - mul two int
+* @a: first int
+* @b: second int
+* 
+* Return: multiply
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - divide two int
+* @a: first int
+* @b: second int
+* 
+* Return: division
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - mod two int
+* @a: first int
+* @b: second int
+* 
+* Return: mod
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
