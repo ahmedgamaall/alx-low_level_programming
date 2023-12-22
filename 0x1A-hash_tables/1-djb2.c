6ff247122ac4977df739ccad704aab909d6f074d
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2023-12-22 05:51:44 -0800

    create 1-djb2.c file

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - function to compute hash of 'str' using djb2
+ * @str: string to hash
+ *
+ * Return: value of 64 bits hash
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
